bc51cf1d276b7a712027aa1e1847ea525e00abc7
KevinGurden <kevin.gurden@googlemail.com>
2014-06-21 14:31:36 +0100

    Updated config " problem

--- php/db_config.php
+++ php/db_config.php
@@ -5,7 +5,7 @@
  */
 
 define('DB_USER', "L2SPlayer"); // db user
-define('DB_PASSWORD', "TheKinks"); // db password (mention your db password here)
-define('DB_DATABASE',songlink"); // database name
+define('DB_PASSWORD', ""); // db password (mention your db password here)
+define('DB_DATABASE',"songlink"); // database name
 define('DB_SERVER', "link2songs.ctjcbtphimqf.us-west-2.rds.amazonaws.com"); // db server
 ?>
