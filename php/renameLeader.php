c3333a44f460efe6aa31b507d9a5a3e85513abc0
KevinGurden <kevin.gurden@googlemail.com>
2014-06-24 12:02:39 +0100

    Debug rename

--- php/renameLeader.php
+++ php/renameLeader.php
@@ -4,7 +4,7 @@
  */
 header('Access-Control-Allow-Origin: *');
 
-$newName = $_GET['name'];
+$newname = $_GET['name'];
 $uuid = $_GET['uuid'];
     
 // Array for JSON response
@@ -17,24 +17,24 @@ require_once __DIR__ . '/db_connect.php';
 $db = new DB_CONNECT();
 
 // Get link from the links table
-$select = "UPDATE leaders SET name=$newName WHERE uuid=$uuid";
+$select = "UPDATE leaders SET name='$newname' WHERE uuid=$uuid";
 $result = mysql_query($select) or die(mysql_error());
 
-// check for empty result
-if (mysql_num_rows($result) > 0) {
-    header('Content-Type: application/json');
-    $response["success"] = 1;
-
-    // echoing JSON response
-    echo json_encode($response);
-} else {
-    header('Content-Type: application/json');
-    $response["success"] = 0;
-    $response["message"] = "No links found ($select)";
-    header('Content-Type: text/html');
-    error_log($response["message"]);
-
-    // echo no users JSON
-    echo json_encode($response);
+//// check for empty result
+//if (mysql_num_rows($result) > 0) {
+//    header('Content-Type: application/json');
+//    $response["success"] = 1;
+//
+//    // echoing JSON response
+//    echo json_encode($response);
+//} else {
+//    header('Content-Type: application/json');
+//    $response["success"] = 0;
+//    $response["message"] = "No links found ($select)";
+//    header('Content-Type: text/html');
+//    error_log($response["message"]);
+//
+//    // echo no users JSON
+//    echo json_encode($response);
 }
 ?>
