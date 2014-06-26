444388afc233c0362a98132fb84aa25fa5e43b31
KevinGurden <kevin.gurden@googlemail.com>
2014-06-26 12:53:38 +0100

    myqli change

--- php/getSongLinks.php
+++ php/getSongLinks.php
@@ -30,9 +30,9 @@ error_log("getSongLinks query");
 // Get link from the links table
 if ($avoidLTs!="") {$avoidLTs = "AND LOCATE(linktype, '$avoidLTs')=0";};
 if ($avoidSongs!="") {$avoidSongs = "AND LOCATE(songidB, '$avoidSongs')=0";};
-$artist = mysqli_real_escape_string($artist);  // Get rid of any single quotes first
+$artist = mysqli_real_escape_string($db, $artist);  // Get rid of any single quotes first
 $select = "SELECT * FROM links WHERE songidA=$songid AND artistA='$artist' AND (difficulty BETWEEN $difflow AND $diffhigh) $avoidLTs $avoidSongs";
-$result = mysql_query($select) or die(mysql_error());
+$result = mysqli_query($db, $select) or die(mysql_error());
 
 error_log("getSongLinks done query");
     
