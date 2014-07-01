3b10a1ae7dedd0c520c88e47f0a1b638ef08b9fa
KevinGurden <kevin.gurden@googlemail.com>
2014-07-01 14:35:37 +0100

    debug

--- php/getSongLinks.php
+++ php/getSongLinks.php
@@ -21,7 +21,7 @@ require_once __DIR__ . '/db_config.php';
 // connecting to db
 $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
 if (mysqli_connect_errno()) {
-    error_log("Failed to connect to MySQL: " . mysqli_connect_error());
+    error_log("Failed to connect to MySQL: " . mysqli_connect_error($con));
 };
 
 error_log("getSongLinks query");
