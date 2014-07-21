5d4b45366d03e25ebb99b1d81bba67d5a0e3ef13
KevinGurden <kevin.gurden@googlemail.com>
2014-07-21 13:39:54 +0100

    Debug

--- php/renameLeader.php
+++ php/renameLeader.php
@@ -16,11 +16,11 @@ require_once __DIR__ . '/db_config.php';
 // connecting to db
 $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
 if (mysqli_connect_errno()) {
-    error_log("Failed to connect to MySQL: " . mysqli_connect_error($con));
+    error_log("Failed to connect to MySQL: " . mysqli_connect_error());
 };
 
 
 // Get link from the links table
 $select = "UPDATE leaders SET name='$newname' WHERE uuid='$uuid'";
-$result = mysqli_query($con, $select) or die(mysqli_error());
+$result = mysqli_query($con, $select) or die(mysqli_error($con));
 ?>
