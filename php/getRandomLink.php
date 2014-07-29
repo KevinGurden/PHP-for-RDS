7cc86408634272f1f371e9527b29413e5fe274fd
KevinGurden <kevin.gurden@googlemail.com>
2014-07-29 14:42:09 +0100

    Debug v

--- php/getRandomLink.php
+++ php/getRandomLink.php
@@ -4,11 +4,12 @@
  */
 header('Access-Control-Allow-Origin: *');
 
-if (isset($_GET['v'])) {$version = $_GET['v'];} else {$version = 0;};    
+if (isset($_GET['v'])) {$version = $_GET['v'];} else {$version = 0;};
 $diff = $_GET['diff'];
 $diffhigh = $diff + 0.2;  // Set higher limit on difficulty
 $difflow = $diff - 0.2;   // .. and for lower
-
+error_log($response["v=$version"]);
+    
 // Array for JSON response
 $response = array();
 
